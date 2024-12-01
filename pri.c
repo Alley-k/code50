40097f9bf1c1428b17f2b89ec8ed48e112373f2f
Alley-k <alley4228p@gmail.com>
2024-12-01 22:45:44 +0530

    Create pri.c

---
+++ pri.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int lenght);
+int main(void)
+
+{
+    int height = get_int("Height: ");
+
+    for (int i = 0; i < height ;i++)
+    {
+        print_row(i + 1 );
+    }
+}
+   void print_row(int lenght)
+{
+
+   for (int i = 0; i < lenght; i++ )
+ {
+    printf("#");
+  }
+  printf("\n");
+ }

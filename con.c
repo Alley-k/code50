84bc0bc20b9c3909abe1de2756ae13d57954fdd2
Alley-k <alley4228p@gmail.com>
2024-12-01 22:37:38 +0530

    Create con.c

---
+++ con.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void);
+
+
+int x = GetInt();
+ switch(x)
+  {
+    case 1 :
+        printf("One\n");
+        break;
+    case 2 :
+        printf("two\n");
+        break;
+    case 3 :
+        printf("three\n");
+    case 4 :
+        printf("Blast-off\n");
+
+
+
+}
